f76b39bfe00e3da5976c6f7dc4e10438d4a400ab
harden <harden@shundaojia.com>
2020-10-12 17:49:21 +0800

    Lambda exercise
    Signed-off-by: harden <harden@shundaojia.com>

--- test2.py
+++ test2.py
@@ -1,6 +1,18 @@
 lst = [lambda x: i * x for i in range(4)]
+
 print([m(2) for m in lst])
-# how to print [0, 2 ,4 ,6] ?
+
+# how to print [0, 2 ,4 ,6] ? You must store i in a local variable
+# method 1
+lst = [lambda x=i, y=i: y * x for i in range(4)]
+print([m(2) for m in lst])
+
+# method 2
+def create_lamdba(x):
+    return lambda y: x * y
+lst = [create_lamdba(i) for i in range(4)]
+print([m(2) for m in lst])
+
 print()
 
 def generator():
