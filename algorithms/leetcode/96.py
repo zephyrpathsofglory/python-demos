8e732c05cf2610ed09f54ea986c53bce7706733e
zephyrpathsofglory <zhangyonguu@gmail.com>
2024-05-05 01:49:42 +0800

    add leetcode exams

---
+++ algorithms/leetcode/96.py
@@ -0,0 +1,26 @@
+"""
+https://leetcode.cn/problems/unique-binary-search-trees/description/
+
+给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的
+二叉搜索树的种数。
+
+给定一个有序序列 1⋯n，为了构建出一棵二叉搜索树，我们可以遍历每个数字 i，将该数字作为
+树根，将 1⋯(i−1) 序列作为左子树，将 (i+1)⋯n 序列作为右子
+树。接着我们可以按照同样的方式递归构建左子树和右子树。
+
+"""
+
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        states = [0] * (n + 1)
+        states[0], states[1] = 1, 1
+
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                states[i] += states[j - 1] * states[i - j]
+
+        return states[n]
+
+
+assert Solution().numTrees(3) == 5
---
+++ algorithms/leetcode/interview_0412.py
@@ -0,0 +1,96 @@
+# https://leetcode.cn/problems/paths-with-sum-lcci/description/
+
+"""
+给定一棵二叉树，其中每个节点都含有一个整数数值(该值或正或负)。设计一个算法，打印节点数值总和等于某个给定值的所有路径的数量。
+注意，路径不一定非得从二叉树的根节点或叶节点开始或结束，但是其方向必须向下(只能从父节点指向子节点方向)。
+
+示例:
+给定如下二叉树，以及目标和 sum = 22，
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+返回:
+
+3
+解释：和为 22 的路径有：[5,4,11,2], [5,8,4,5], [4,11,7]
+提示：
+
+节点总数 <= 10000
+"""
+import collections
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        def rootSum(root: TreeNode, sum):
+            if root is None:
+                return 0
+
+            ret = 0
+            if root.val == sum:
+                ret += 1
+
+            ret += rootSum(root.left, sum - root.val)
+            ret += rootSum(root.right, sum - root.val)
+
+            return ret
+
+        ret = rootSum(root, sum)
+
+        if root is None:
+            return ret
+        ret += self.pathSum(root.left, sum)
+        ret += self.pathSum(root.right, sum)
+
+        return ret
+
+
+root = TreeNode(5)
+root.left = TreeNode(4)
+root.right = TreeNode(8)
+root.left.left = TreeNode(11)
+root.left.left.left = TreeNode(7)
+root.left.left.right = TreeNode(2)
+root.right.left = TreeNode(13)
+root.right.right = TreeNode(4)
+root.right.right.left = TreeNode(5)
+root.right.right.right = TreeNode(1)
+
+assert Solution().pathSum(root, 22) == 3
+
+
+class Solution2:
+    def pathSum(self, root: TreeNode, sum: int):
+        prefix = collections.defaultdict(int)
+        prefix[0] = 1
+
+        def dfs(root: TreeNode, curr: int):
+            if root is None:
+                return 0
+
+            ret = 0
+            curr += root.val
+            ret += prefix[curr - sum]
+            prefix[curr] += 1
+            ret += dfs(root.left, curr)
+            ret += dfs(root.right, curr)
+            prefix[curr] -= 1
+
+            return ret
+
+        return dfs(root, 0)
+
+
+assert Solution2().pathSum(root, 22) == 3
